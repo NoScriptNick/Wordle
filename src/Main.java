168080864c296838383d62a96516e4c8d7cd2314
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2024-02-15 08:06:18 -0500

    sadgbbbasdfaef

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,22 @@ public class Main {
 
         //variables
         ArrayList<String> wordList = new ArrayList<String>();
-        wordList.addAll(Arrays.asList("Scram", "Dream", "Cream", "Trust", "Conquest", "Bus", "Stream", "Bloom", "Crest", "Grown", "Quest", "MrBoolean"))
+        wordList.addAll(Arrays.asList("scram", "dream", "cream", "trust", "conquest", "bus", "stream", "bloom", "crest", "grown", "quest", "mrboolean"));
         ArrayList<String> filteredList = new ArrayList<String>();
+        String wordle;
         String guess;
         int wordleIndex;
+
+        for (int i = 0; i < wordList.size(); i++) {
+            if (wordList.get(i).length() == 5) {
+                filteredList.add(wordList.get(i));
+            }
+        }
+
+        wordleIndex = rand.nextInt(filteredList.size());
+        wordle = filteredList.get(wordleIndex);
+
+        System.out.println(wordle);
 
         //5 letters
         //letter turns yellow if it is in the word but not in the right spot
@@ -34,16 +46,20 @@ public class Main {
                 "word AND the right place, it will show up green! The game will end when you've guessed the word. Good luck! ");
         System.out.println("_____");
 
-        guess = scan.nextLine();
-        wordleIndex = rand.nextInt(wordList.size());
+        for (int i = 0; i < 5; i++) {
+            guess = InputHelper.getLengthFiveString(scan, "Enter a word!");
+                for (int j = 0; j < wordle.length(); j++) {
+                    if (guess.substring(j, j+1).equalsIgnoreCase(wordle.substring(j, j+1))) {
+                        System.out.print(greenBackground + guess.substring(j, j+1) + resetBackground);
+                    } else if (guess.substrubg) {
 
-        for (int i = 0; i < wordList.size(); i++) {
-            if (wordList.get(i).length() == 5) {
-                filteredList.add(wordList.get(i));
-            }
+                    } else {
+                        System.out.print(resetBackground + guess.substring(j, j+1));
+                    }
+                }
+            System.out.println("");
+            System.out.println("Try another word!");
         }
-
-
 
     }
 }
